bcbfdf7266eb9f8d67d59a2fd56c9cccf76374cb
mimikun <karaage_nennen@icloud.com>
2019-07-27 02:50:04 +0900

    cleanup dir

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-pandoc -f markdown -t html day1.md -s -o day1.html --metadata pagetitle="day1"
-pandoc -f markdown -t html day3.md -s -o day3.html --metadata pagetitle="day3"
-pandoc -f markdown -t html day4.md -s -o day4.html --metadata pagetitle="day4"
