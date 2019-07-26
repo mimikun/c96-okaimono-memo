278d13d1d0c84e6cac5ecc4c9a6d15b336b89c0c
mimikun <karaage_nennen@icloud.com>
2019-07-27 02:53:35 +0900

    test

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+git checkout -b gh-pages
+
 pandoc -f markdown -t html day1.md -s -o docs/day1.html --metadata pagetitle="day1"
 pandoc -f markdown -t html day3.md -s -o docs/day3.html --metadata pagetitle="day3"
 pandoc -f markdown -t html day4.md -s -o docs/day4.html --metadata pagetitle="day4"
@@ -12,7 +14,7 @@ git config --global user.email "<>"
 git add -A
 git commit -m "[ci skip] Deploy by CI"
 git fetch origin
-git checkout -b gh-pages
+
 git push -f origin gh-pages:gh-pages
 
 echo "Deploy a site!"
