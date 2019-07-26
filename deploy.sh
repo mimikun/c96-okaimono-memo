46ffd2cb0aff36550dfe89a95881d697be4e4f7f
mimikun <karaage_nennen@icloud.com>
2019-07-27 01:55:09 +0900

    fix

--- deploy.sh
+++ deploy.sh
@@ -12,8 +12,6 @@ git config --global user.email "<>"
 git add -A
 git commit -m "[ci skip] Deploy by CI"
 
-git push -f $(git config --get remote.origin.url) master:master > /dev/null/ 2>&1
+git push -f $(git config --get remote.origin.url) master:master
 
 echo "Deploy a site!"
-
-cd -
