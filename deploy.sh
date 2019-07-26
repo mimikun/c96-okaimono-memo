20448c6468c9d7c6a3c6d1408d29af96ff444eef
mimikun <karaage_nennen@icloud.com>
2019-07-27 02:42:02 +0900

    hoge

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,10 @@ fi
 
 git config --global user.name "Circle CI"
 git config --global user.email "<>"
-git checkout gh-pages
 git add -A
 git commit -m "[ci skip] Deploy by CI"
-git remote -v
-git push origin gh-pages:gh-pages
+git fetch origin
+git checkout gh-pages
+git push -f origin gh-pages:gh-pages
 
 echo "Deploy a site!"
