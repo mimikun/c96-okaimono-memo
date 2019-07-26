551448254591aeb4914af241cdee9e9232e066ef
mimikun <karaage_nennen@icloud.com>
2019-07-27 01:41:37 +0900

    fix

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-pandoc -f markdown -t html day1.md -s -o ./pages/day1.html --metadata pagetitle="day1"
-pandoc -f markdown -t html day3.md -s -o ./pages/day3.html --metadata pagetitle="day3"
-pandoc -f markdown -t html day4.md -s -o ./pages/day4.html --metadata pagetitle="day4"
+pandoc -f markdown -t html day1.md -s -o day1.html --metadata pagetitle="day1"
+pandoc -f markdown -t html day3.md -s -o day3.html --metadata pagetitle="day3"
+pandoc -f markdown -t html day4.md -s -o day4.html --metadata pagetitle="day4"
