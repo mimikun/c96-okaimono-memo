86c6ea15f0c7af29d9721e7b2737b91f48962310
mimikun <karaage_nennen@icloud.com>
2019-07-27 01:51:11 +0900

    fix

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+pandoc -f markdown -t html day1.md -s -o pages/day1.html --metadata pagetitle="day1"
+pandoc -f markdown -t html day3.md -s -o pages/day3.html --metadata pagetitle="day3"
+pandoc -f markdown -t html day4.md -s -o pages/day4.html --metadata pagetitle="day4"
+
+if [ "$(git status --porcelain | wc -l | xargs)" -eq 0 ]; then
+  echo "Not exist deploying contents."
+  exit 0
+fi
+
+git config --global user.name "Circle CI"
+git config --global user.email "<>"
+git add -A
+git commit -m "[ci skip] Deploy by CI"
+
+git push -f $(git config --get remote.origin.url) master:master > /dev/null/ 2>&1
+
+echo "Deploy a site!"
+
+cd -
